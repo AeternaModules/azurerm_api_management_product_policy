47a43c2edadf84b885e0827df269b67af669c578
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "api_management_product_policies_id" {
+  description = "Map of id values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.id }
+}
 output "api_management_product_policies_api_management_name" {
   description = "Map of api_management_name values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
   value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.api_management_name }
