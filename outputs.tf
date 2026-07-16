3e49948dc1a974ec844dbaa7d475e12c43d6d705
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "api_management_product_policies_id" {
   description = "Map of id values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
-  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.id }
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_product_policies_api_management_name" {
   description = "Map of api_management_name values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
-  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.api_management_name }
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.api_management_name if v.api_management_name != null && length(v.api_management_name) > 0 }
 }
 output "api_management_product_policies_product_id" {
   description = "Map of product_id values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
-  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.product_id }
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.product_id if v.product_id != null && length(v.product_id) > 0 }
 }
 output "api_management_product_policies_resource_group_name" {
   description = "Map of resource_group_name values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
-  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.resource_group_name }
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "api_management_product_policies_xml_content" {
   description = "Map of xml_content values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
-  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.xml_content }
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.xml_content if v.xml_content != null && length(v.xml_content) > 0 }
 }
 output "api_management_product_policies_xml_link" {
   description = "Map of xml_link values across all api_management_product_policies, keyed the same as var.api_management_product_policies"
-  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.xml_link }
+  value       = { for k, v in azurerm_api_management_product_policy.api_management_product_policies : k => v.xml_link if v.xml_link != null && length(v.xml_link) > 0 }
 }
 
